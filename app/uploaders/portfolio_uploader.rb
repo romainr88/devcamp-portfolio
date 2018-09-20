c1d5329200635df4f9197795575ffa403d734d57
romainr88 <romainr88dev@gmail.com>
2018-09-20 18:51:14 +0000

    Integrated cocoon for portfolio items nested form component

--- app/uploaders/portfolio_uploader.rb
+++ app/uploaders/portfolio_uploader.rb
@@ -1,9 +1,13 @@
 class PortfolioUploader < CarrierWave::Uploader::Base
-   storage :aws
+  
+  storage :aws
+  
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+  
   def extension_whitelist
     %w(jpg jpeg gif png)
   end
+  
 end
