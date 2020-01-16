9abcc0f9b343048c4578b60bb99999ed67483af1
romainr88 <romainr88dev@gmail.com>
2020-01-16 15:25:14 +0100

    Updated source tracking feature

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,17 +9,17 @@ module ApplicationHelper
     end
   end
 
-  def source_helper(layout_name)
+  def source_helper(styles)
     if session[:source]
-      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
-      content_tag(:p, greeting, class: "source-greeting")
+      greeting = "Thanks for visiting me from #{session[:source]}, please feel free to #{ link_to 'contact me', contact_path } if you'd like to work together."
+      content_tag(:div, greeting.html_safe, class: styles)
     end
   end
 
   def copyright_generator
     DevcampViewTool::Renderer.copyright 'Jordan Hudgens', 'All rights reserved'
   end
-  
+
   def nav_items
     [
       {
@@ -48,29 +48,29 @@ module ApplicationHelper
       }
     ]
   end
-  
+
   def nav_helper style, tag_type
     nav_links = ''
-    
+
     nav_items.each do |item|
       nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
     end
 
     nav_links.html_safe
   end
-  
+
   def active? path
     "active" if current_page? path
   end
-  
+
   def alerts
     alert = (flash[:alert] || flash[:error] || flash[:notice])
-    
+
     if alert
       alert_generator alert
     end
   end
-  
+
   def alert_generator msg
       js add_gritter(msg, title: "Jordan Hudgens Portfolio", sticky: false)
   end
